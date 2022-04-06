0de4a2170792a510bdd353337b106db41b30fb49
Lucas Sawade <lsawade@princeton.edu>
2022-04-06 16:23:43 -0400

    added some things

--- solver.py
+++ solver.py
@@ -2,24 +2,33 @@ import toml
 from radical.entk import Pipeline, Stage, Task, AppManager
 import traceback, sys, os
 
+# Load config
+cfg = toml.load('entk.toml')
 
+# Set radical environment flags
+for key, value in cfg['radical']['env'].items():
+    os.environ[key] = str(value)
 
-entkcfg = tompl.load('entk.toml')
+# Setup rabbitmq
+rmq = cfg['RMQ']
+hostname = rmq['HOSTNAME']
+port = rmq['PORT']
+password = rmq['PASSWORD']
+username = rmq['USERNAME']
 
-hostname = entkcfg['RMQ_HOSTNAME']
-port = entkcfg['RMQ_PORT']
+# Set environment URL
+os.environ["RADICAL_PILOT_DBURL"] = f"mongodb://{username}:{password}@{hostname}/specfm"
 
-
-password = entkcfg['RMQ_PASSWORD']
-username = entkcfg['RMQ_USERNAME']
-
+# Print setup
 print("Setup:\n--------")
 print(hostname)
 print(port)
 print(password[:2] + "*" * (len(password)-4) + password[-2:])
 print(username)
 
-specfem = "/scratch/gpfs/lsawade/SpecfemMagic/specfem3d_globe"
+specfem = "/gpfs/alpine/geo111/scratch/lsawade/SpecfemMagic/specfem3d_globe"
+
+
 
 if __name__ == '__main__':
     p = Pipeline()
@@ -30,7 +39,9 @@ if __name__ == '__main__':
 
     # Create 'Hello world' task
     t = Task()
-    t.pre_exec = ['module load spectrum-mpi gcc']
+    t.pre_exec = [
+        '. /sw/summit/lmod/lmod/init/profile',
+        'module load spectrum-mpi gcc']
     t.name = "HelloWorldTask"
     t.executable = '/bin/echo'
     t.arguments = ['"Hello world"']
@@ -42,52 +53,42 @@ if __name__ == '__main__':
     test_stage.add_tasks(t)
     p.add_stages(test_stage)
        
-    # specfem_stage = Stage()
-    # specfem_stage.name = 'SimulationStage'
+    specfem_stage = Stage()
+    specfem_stage.name = 'SimulationStage'
     
-    # for i in range(2):
+    for i in range(2):
 
-    #     # Create Task
-    #     t = Task()
-    #     t.name = f"SIMULATION.{i}"
-    #     tdir = f"/home/lsawade/simple_entk_specfem/specfem_run_{i}"
-    #     t.pre_exec = [
-    #         # Load necessary modules
-    #         'module load openmpi/gcc',
-    #         'module load cudatoolkit',
-            
-    #         # Change to your specfem run directory
-    #         f'rm -rf {tdir}',
-    #         f'mkdir {tdir}',
-    #         f'cd {tdir}',
-            
-    #         # Create data structure in place
-    #         f'ln -s {specfem}/bin .',
-    #         f'ln -s {specfem}/DATABASES_MPI .',
-    #         f'cp -r {specfem}/OUTPUT_FILES .',
-    #         'mkdir DATA',
-    #         f'cp {specfem}/DATA/CMTSOLUTION ./DATA/',
-    #         f'cp {specfem}/DATA/STATIONS ./DATA/',
-    #         f'cp {specfem}/DATA/Par_file ./DATA/'
-    #     ]
-    #     t.executable = './bin/xspecfem3D'
-    #     t.cpu_reqs = {'cpu_processes': 4, 'cpu_process_type': 'MPI', 'cpu_threads': 1, 'cpu_thread_type' : 'OpenMP'}
-    #     t.gpu_reqs = {'gpu_processes': 1, 'gpu_process_type': None, 'gpu_threads': 1, 'gpu_thread_type' : 'CUDA'}
-    #     t.stdout = 'STDOUT'
-    #     t.stderr = 'STDERR'
-    #     t.download_output_data = ['STDOUT', 'STDERR']
+        # Create Task
+        t = Task()
+        t.name = f"SIMULATION.{i:0>2d}"
+        tdir = f"/gpfs/alpine/geo111/scratch/lsawade/entk_test/specfem_run_{i}"
+        # t.sandbox = tdir
+        t.pre_exec = [
+            '. /sw/summit/lmod/lmod/init/profile',
+            # Load necessary modules
+            'module purge',
+            'module load gcc spectrum-mpi cuda cmake boost',
+            f'cd {tdir}'
+        ]
+        t.executable = './bin/xspecfem3D'
+        t.cpu_reqs = {'cpu_processes': 24, 'cpu_process_type': 'MPI', 'cpu_threads': 1, 'cpu_thread_type' : 'OpenMP'}
+        t.gpu_reqs = {'gpu_processes': 1,  'gpu_process_type': None,  'gpu_threads': 1, 'gpu_thread_type' : 'CUDA'}
+        t.stdout = 'STDOUT'
+        t.stderr = 'STDERR'
+        t.download_output_data = ['STDOUT', 'STDERR']
 
-    #     # Add task to stage
-    #     specfem_stage.add_tasks(t)
+        # Add task to stage
+        specfem_stage.add_tasks(t)
         
-    # # p.add_stages(specfem_stage)
+    p.add_stages(specfem_stage)
         
     res_dict = {
-        'resource': '', # 'local.localhost',
-        'schema'   : 'local',
+        'resource': 'ornl.summit_prte', # 'local.localhost',
+        'schema' : 'local',
         'walltime':  20, #2 * 30,
-        'cpus': 2 * 4 + 4,
-        'gpus': 2 * 4,          
+        'cpus': 48,
+        'gpus': 48,
+        'project': 'GEO111'
     }
 
     appman = AppManager(hostname=hostname, port=port, username=username, password=password, resubmit_failed=False)
